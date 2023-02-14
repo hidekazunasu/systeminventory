8b43ba3ad75f08b50c112994487b584345509241
root <root@DESKTOP-M1IPQT6.localdomain>
2023-02-14 20:32:19 +0900

    add db file

---
+++ psql/init/create.sql
@@ -0,0 +1,13 @@
+-- テーブルを作成
+CREATE TABLE sample(
+  id integer,
+  division varchar(10),
+  name varchar(10)
+);
+
+-- データを挿入
+INSERT INTO sample VALUES (1, '区分1', '名称1');
+INSERT INTO sample VALUES (2, '区分2', '名称2');
+INSERT INTO sample VALUES (3, '区分3', '名称3');
+INSERT INTO sample VALUES (4, '区分4', '名称4');
+INSERT INTO sample VALUES (5, '区分5', '名称5');
